35d6f51c580164299416780b13656f4e87d438fd
Andre Oliveira <andreluisoliveira013@gmail.com>
2020-02-15 20:19:08 -0300

    upando um codigo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity} from 'react-native';
 
 // import { Container } from './styles';
 
 export default function Login() {
   return (
-    <View />
+    <View>
+      <View>
+
+      </View>
+
+      <View>
+        <View>
+          <Text>Email</Text>
+          <TextInput></TextInput>
+        </View>
+        <View>
+          <Text>Senha</Text>
+          <TextInput></TextInput>
+        </View>
+
+      </View>
+
+    </View>
   );
 }
