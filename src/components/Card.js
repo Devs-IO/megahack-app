186f2e5b66a63f83046ebf75b242eb767ece9182
lucaspontoexe <ltx16@hotmail.com>
2020-02-16 00:44:11 -0300

    iniciando port do card do react web

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+import styled from "styled-components/native";
 
-import styled from 'styled-components/native';
+import visa from "../../assets/cards/visa.png";
+import master from "../../assets/cards/mastercard.svg";
+import eye from "../../assets/icons/olho.svg";
 
-export default function Card(props) {
+const cardtypes = {
+  visa,
+  master
+};
+
+const Container = styled.View`
+  width: 136px;
+  border-radius: 10px;
+  background-color: #f2f2f2;
+`;
+
+const CardContainer = styled.View`
+  display: grid;
+  grid-template-columns: 30px auto 20px;
+  grid-template-rows: 20px auto 25px;
+  height: 70px;
+  background-color: #347474;
+  border-radius: 10px;
+  padding: 8px;
+`;
+
+const Limit = styled.View`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-content: flex-end;
+
+  padding: 0px 8px;
+`;
+
+export default function Card({ type, end, limit }) {
   return (
-    <View style={}>
-      <Text>cartão</Text>
-    </View>
+    <Container>
+      <CardContainer>
+        <Image source={cardtypes[type]} />
+        <Image source={eye} />
+        <Text>**** **** **** {end}</Text>
+      </CardContainer>
+      <Limit>
+        <Text>R$ {limit}</Text>
+        <Text> de limite</Text>
+      </Limit>
+    </Container>
   );
 }
