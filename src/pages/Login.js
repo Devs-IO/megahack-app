c0f304ec22d5db10228a2779749bad83d1eee1fb
Andre Oliveira <andreluisoliveira013@gmail.com>
2020-02-15 23:03:57 -0300

    tela de login pronta

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,9 +6,8 @@ import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native
 export default function Login() {
   return (
     <View style={styles.container}>
-      
-
-      <View style={styles.divSuperior}>   
+      <View style={styles.divSuperior}>
+        <Text style={styles.TextLogin}>Login</Text>   
       </View>
       <View style={styles.retanguloUm}>
       </View>
@@ -17,7 +16,39 @@ export default function Login() {
       <View style={styles.retanguloTres}>
       </View>
       <View style={styles.retanguloQuatro}>
+
+        <View style={styles.ViewInput}>
+          <Text style={styles.TextInput}>Email</Text>
+          <TextInput style={styles.Input} keyboardType='email-address' maxLength={30}
+        autoCapitalize='none'></TextInput>
+          <View style={styles.ViewFina}></View>
+        </View>
+
+        <View style={styles.ViewInput}>
+          <Text style={styles.TextInput}>Senha</Text>
+          <TextInput style={styles.Input} secureTextEntry={true} autoCapitalize='none'
+          style={styles.TextInput}></TextInput>
+          <View style={styles.ViewFina}></View>
+        </View>
+
+        <TouchableOpacity style={styles.Botao}>
+          <Text style={styles.TextEntrar} >Entrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.BotaoCadastrar} >
+          <Text style={styles.TextCadastrar}>Cadastra-se</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text style={styles.TextCadastrar}>Esqueci minha senha</Text>
+        </TouchableOpacity>
+
       </View>
+
+      
+
+      
+      
 
       
 
@@ -50,7 +81,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     transform:([{ rotateX: '20deg' }, { rotateZ: '2.3rad' }, {rotateY: '10deg'}]),
     marginLeft:250,
-    marginTop:300,
+    marginTop:250,
     flexDirection:'column',
     zIndex:1
   },
@@ -60,30 +91,90 @@ const styles = StyleSheet.create({
     backgroundColor:'#EFEFEF',
     display: 'flex',
     position: 'absolute',
-    marginTop:326,
+    marginTop:300,
     marginLeft:115,
     transform:([{ rotateX: '20deg' }, { rotateZ: '0.45rad' }, {rotateY: '10deg'}]),
     zIndex:2
   },
   retanguloTres:{
-    width:'50%',
+    width:'60%',
     height:'20%',
     backgroundColor:'#EFEFEF',
     display: 'flex',
     position: 'absolute',
-    marginTop:330,
-    marginLeft:10,
-    transform:([{ rotateX: '20deg' }, { rotateZ: '2.6rad' }, {rotateY: '10deg'}]),
+    marginTop:320,
+    marginLeft:-17,
+    transform:([{ rotateX: '20deg' }, { rotateZ: '2.5rad' }, {rotateY: '10deg'}]),
     zIndex:2
   },
   retanguloQuatro:{
     width:'100%',
-    height:'20%',
+    height:'60%',
     backgroundColor:'#EFEFEF',
     display: 'flex',
-    marginTop:420,
+    justifyContent:'flex-start',
+    alignItems:'center',
+    marginTop:390,
     position: 'absolute',
     zIndex:2
-  }
+  },
+  TextLogin:{
+    color:'#FFF',
+    fontSize:36,
+    fontWeight:'700',
+    marginBottom:100
+  },
+  ViewInput:{
+    width:'70%',
+    height:'12%',
+    justifyContent:'center',
+    alignItems:'flex-start',
+    marginBottom:40,
+    zIndex:4
+  },
+  Input:{
+    width:'100%',
+    height:'80%',
+  },
+  TextInput:{
+    color:'#323232',
+    fontSize:26,
+    fontWeight:'700',
+  },
+  ViewFina:{
+    width:'100%',
+    height:'5%',
+    backgroundColor:'#323232'
+  },
+  Botao:{
+    width:'70%',
+    height:'12%',
+    backgroundColor: '#347474',
+    borderRadius: 10,
+    marginBottom:10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex:4
+  },
+  TextEntrar:{
+    color:'#ffff',
+    fontSize:20,
+    fontWeight:'700',
+  },
+  BotaoCadastrar:{
+    width:'70%',
+    height:'12%',
+    backgroundColor: '#ffff',
+    borderRadius: 10,
+    marginBottom:10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex:4
+  },
+  TextCadastrar:{
+    color:'#323232',
+    fontSize:20,
+    fontWeight:'700',
+  },
 
 });
