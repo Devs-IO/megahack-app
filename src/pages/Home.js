068ab1908be60b274dad5d1d2e77ed09fdc92d22
lucaspontoexe <ltx16@hotmail.com>
2020-02-17 23:50:14 -0300

    add state, buttons and formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,157 +1,160 @@
-import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function Home() {
+export default function Home({navigation}) {
   return (
     <View style={styles.container}>
-     <View style={styles.viewSuperior}></View>
-     <View style={styles.viewDados}>
+      <View style={styles.viewSuperior}></View>
+      <View style={styles.viewDados}>
+        <View style={styles.viewSaldo}>
+          <View style={styles.viewVisualSaldo}>
+            <View style={styles.viewTextos}>
+              <Text style={styles.TextSaldoEmConta}>Saldo em conta</Text>
+              <Text style={styles.TextSaldo}>R$ 1.500,62</Text>
+            </View>
+            <View style={styles.viewBotaoOlho}></View>
+          </View>
+          <View style={styles.viewBotoesSaldo}>
+            <TouchableOpacity>
+              <Text>Guardar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text>Retirar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={navigation.navigate('Pay')}>
+              <Text>Pagar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text>Minha Carteira</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View style={styles.viewCartao}></View>
+        <View style={styles.viewBorda}></View>
 
-       <View style={styles.viewSaldo}>
-         <View style={styles.viewVisualSaldo}>
-           <View style={styles.viewTextos}>
-             <Text style={styles.TextSaldoEmConta}>Saldo em conta</Text>
-             <Text style={styles.TextSaldo}>R$ 1.500,62</Text>
-           </View>
-           <View style={styles.viewBotaoOlho}>
-           </View>       
-         </View>
-         <View style={styles.viewBotoesSaldo}>
-           <TouchableOpacity>
-             <Text></Text>
-           </TouchableOpacity>
-           <TouchableOpacity>
-             <Text></Text>
-           </TouchableOpacity>
-           <TouchableOpacity>
-             <Text></Text>
-           </TouchableOpacity>
-
-
-         </View>
-         
-         
-
-
-       </View>
-       <View style={styles.viewCartao}></View>
-       <View style={styles.viewBorda}></View>
-       
-       
-
-     </View>
-      
+        <View>
+          <TouchableOpacity>
+            <Text>Depositar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Pagar</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Despesas</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text>Registros</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EFEFEF',
+    backgroundColor: "#EFEFEF"
   },
-  viewSuperior:{
-    width:'100%',
-    height:'30%',
-    backgroundColor:'#494949',
-    display: 'flex',
-    flexDirection:'row',
-    zIndex:1
+  viewSuperior: {
+    width: "100%",
+    height: "30%",
+    backgroundColor: "#494949",
+    display: "flex",
+    flexDirection: "row",
+    zIndex: 1
   },
-  viewDados:{
-    width:'100%',
-    height:'50%',
-    display: 'flex',
-    flexDirection:'column',
+  viewDados: {
+    width: "100%",
+    height: "50%",
+    display: "flex",
+    flexDirection: "column"
   },
-  viewSaldo:{
-    width:'100%',
-    height:'35%',
-    backgroundColor:'#ffff',
-    display: 'flex',
-    flexDirection:'column',
-    borderTopLeftRadius:30,
-    borderBottomRightRadius:30,
-    zIndex:3,
+  viewSaldo: {
+    width: "100%",
+    height: "35%",
+    backgroundColor: "#ffff",
+    display: "flex",
+    flexDirection: "column",
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    zIndex: 3,
     shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 0.25,
-        elevation: 5,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowOpacity: 0.25,
+    elevation: 5
   },
-  viewCartao:{
-    width:'100%',
-    height:'43%',
-    backgroundColor:'#85B2B2',
-    display: 'flex',
-    flexDirection:'row',
-    borderTopLeftRadius:30,
-    borderBottomRightRadius:30,
-    marginTop:-25,
-    marginBottom:-25,
-    zIndex:2,
+  viewCartao: {
+    width: "100%",
+    height: "43%",
+    backgroundColor: "#85B2B2",
+    display: "flex",
+    flexDirection: "row",
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    marginTop: -25,
+    marginBottom: -25,
+    zIndex: 2,
     shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 0.25,
-        elevation: 4,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowOpacity: 0.25,
+    elevation: 4
   },
-  viewBorda:{
-    width:'100%',
-    height:'35%',
-    backgroundColor:'#FFF',
-    display: 'flex',
-    flexDirection:'row',
-    borderTopLeftRadius:30,
-    borderBottomRightRadius:30,
-    zIndex:1,
+  viewBorda: {
+    width: "100%",
+    height: "35%",
+    backgroundColor: "#FFF",
+    display: "flex",
+    flexDirection: "row",
+    borderTopLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    zIndex: 1,
     shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 0,
-        },
-        shadowOpacity: 0.25,
-        elevation: 3,
+    shadowOffset: {
+      width: 0,
+      height: 0
+    },
+    shadowOpacity: 0.25,
+    elevation: 3
   },
-  viewVisualSaldo:{
-    width:'100%',
-    height:'50%',
-    flexDirection:'row'
+  viewVisualSaldo: {
+    width: "100%",
+    height: "50%",
+    flexDirection: "row"
   },
-  TextSaldoEmConta:{
-    color:'#347474',
-    fontSize:20,
-    
+  TextSaldoEmConta: {
+    color: "#347474",
+    fontSize: 20
   },
-  TextSaldo:{
-    color:'#323232',
-    fontSize:35,
-    fontWeight:'bold',
-    marginTop:-5
+  TextSaldo: {
+    color: "#323232",
+    fontSize: 35,
+    fontWeight: "bold",
+    marginTop: -5
   },
-  viewTextos:{
-    width:'70%',
-    height:'100%',
-    paddingLeft:20,
-    justifyContent:'center'
+  viewTextos: {
+    width: "70%",
+    height: "100%",
+    paddingLeft: 20,
+    justifyContent: "center"
   },
-  viewBotaoOlho:{
-    width:'30%',
-    height:'100%',
-    backgroundColor:'#292929',
-    paddingLeft:20,
-    justifyContent:'center'
+  viewBotaoOlho: {
+    width: "30%",
+    height: "100%",
+    backgroundColor: "#292929",
+    paddingLeft: 20,
+    justifyContent: "center"
   },
-  viewBotoesSaldo:{
-    width:'100%',
-    height:'50%',
-    flexDirection:'row',
-    backgroundColor:'#898989',
-    
-  },
+  viewBotoesSaldo: {
+    width: "100%",
+    height: "50%",
+    flexDirection: "row",
+    backgroundColor: "#898989"
+  }
 });
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,187 +1,204 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
-
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet
+} from "react-native";
 
 // import { Container } from './styles';
 
-
-
 export default function Login({ navigation }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-  state = {
-    email:'',
-    password:''
-  }
-
-  const login = () =>{
-    alert(this.state.email)
-
-
-  }
+  const login = () => {
+    console.log(state);
+  };
 
   return (
     <View style={styles.container}>
       <View style={styles.divSuperior}>
-        <Text style={styles.TextLogin}>Login</Text>   
+        <Text style={styles.TextLogin}>Login</Text>
       </View>
-      <View style={styles.retanguloUm}>
-      </View>
-      <View style={styles.retanguloDois}>
-      </View>
-      <View style={styles.retanguloTres}>
-      </View>
+      <View style={styles.retanguloUm}></View>
+      <View style={styles.retanguloDois}></View>
+      <View style={styles.retanguloTres}></View>
       <View style={styles.retanguloQuatro}>
-
         <View style={styles.ViewInput}>
           <Text style={styles.TextInput}>Email</Text>
-          <TextInput style={styles.Input} keyboardType='email-address' maxLength={30}
-        autoCapitalize='none'></TextInput>
+          <TextInput
+            style={styles.Input}
+            keyboardType="email-address"
+            maxLength={30}
+            autoCapitalize="none"
+            value={email}
+            onChange={setEmail}
+          ></TextInput>
           <View style={styles.ViewFina}></View>
         </View>
 
         <View style={styles.ViewInput}>
           <Text style={styles.TextInput}>Senha</Text>
-          <TextInput style={styles.Input} secureTextEntry={true} autoCapitalize='none'></TextInput>
+          <TextInput
+            style={styles.Input}
+            secureTextEntry={true}
+            autoCapitalize="none"
+            value={password}
+            onChange={setPassword}
+          ></TextInput>
           <View style={styles.ViewFina}></View>
         </View>
 
-        <TouchableOpacity style={styles.Botao} onPress={()=>login()}>
-          <Text style={styles.TextEntrar} >Entrar</Text>
+        <TouchableOpacity style={styles.Botao} onPress={() => login()}>
+          <Text style={styles.TextEntrar}>Entrar</Text>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.BotaoCadastrar} >
+        <TouchableOpacity
+          style={styles.BotaoCadastrar}
+          onPress={() => navigation.navigate("NewAccount")}
+        >
           <Text style={styles.TextCadastrar}>Cadastra-se</Text>
         </TouchableOpacity>
 
         <TouchableOpacity>
           <Text style={styles.TextCadastrar}>Esqueci minha senha</Text>
         </TouchableOpacity>
-
       </View>
-
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    width:'100%',
-    height:'100%',
-    backgroundColor: '#EFEFEF',
-    display: 'flex',
-    flexDirection:'column'
-    
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#EFEFEF",
+    display: "flex",
+    flexDirection: "column"
   },
   divSuperior: {
-    width:'100%',
-    height:'60%',
-    backgroundColor:'#323232',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex:0
+    width: "100%",
+    height: "60%",
+    backgroundColor: "#323232",
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 0
   },
-  retanguloUm:{
-    width:'70%',
-    height:'20%',
-    backgroundColor:'#EFEFEF',
-    display: 'flex',
-    position: 'absolute',
-    transform:([{ rotateX: '20deg' }, { rotateZ: '2.3rad' }, {rotateY: '10deg'}]),
-    marginLeft:250,
-    marginTop:250,
-    flexDirection:'column',
-    zIndex:1
+  retanguloUm: {
+    width: "70%",
+    height: "20%",
+    backgroundColor: "#EFEFEF",
+    display: "flex",
+    position: "absolute",
+    transform: [
+      { rotateX: "20deg" },
+      { rotateZ: "2.3rad" },
+      { rotateY: "10deg" }
+    ],
+    marginLeft: 250,
+    marginTop: 250,
+    flexDirection: "column",
+    zIndex: 1
   },
-  retanguloDois:{
-    width:'50%',
-    height:'20%',
-    backgroundColor:'#EFEFEF',
-    display: 'flex',
-    position: 'absolute',
-    marginTop:300,
-    marginLeft:115,
-    transform:([{ rotateX: '20deg' }, { rotateZ: '0.45rad' }, {rotateY: '10deg'}]),
-    zIndex:2
+  retanguloDois: {
+    width: "50%",
+    height: "20%",
+    backgroundColor: "#EFEFEF",
+    display: "flex",
+    position: "absolute",
+    marginTop: 300,
+    marginLeft: 115,
+    transform: [
+      { rotateX: "20deg" },
+      { rotateZ: "0.45rad" },
+      { rotateY: "10deg" }
+    ],
+    zIndex: 2
   },
-  retanguloTres:{
-    width:'60%',
-    height:'20%',
-    backgroundColor:'#EFEFEF',
-    display: 'flex',
-    position: 'absolute',
-    marginTop:320,
-    marginLeft:-17,
-    transform:([{ rotateX: '20deg' }, { rotateZ: '2.5rad' }, {rotateY: '10deg'}]),
-    zIndex:2
+  retanguloTres: {
+    width: "60%",
+    height: "20%",
+    backgroundColor: "#EFEFEF",
+    display: "flex",
+    position: "absolute",
+    marginTop: 320,
+    marginLeft: -17,
+    transform: [
+      { rotateX: "20deg" },
+      { rotateZ: "2.5rad" },
+      { rotateY: "10deg" }
+    ],
+    zIndex: 2
   },
-  retanguloQuatro:{
-    width:'100%',
-    height:'60%',
-    backgroundColor:'#EFEFEF',
-    display: 'flex',
-    justifyContent:'flex-start',
-    alignItems:'center',
-    marginTop:390,
-    position: 'absolute',
-    zIndex:2
+  retanguloQuatro: {
+    width: "100%",
+    height: "60%",
+    backgroundColor: "#EFEFEF",
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginTop: 390,
+    position: "absolute",
+    zIndex: 2
   },
-  TextLogin:{
-    color:'#FFF',
-    fontSize:36,
-    fontWeight:'700',
-    marginBottom:100
+  TextLogin: {
+    color: "#FFF",
+    fontSize: 36,
+    fontWeight: "700",
+    marginBottom: 100
   },
-  ViewInput:{
-    width:'70%',
-    height:'12%',
-    justifyContent:'center',
-    alignItems:'flex-start',
-    marginBottom:40,
-    zIndex:4
+  ViewInput: {
+    width: "70%",
+    height: "12%",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    marginBottom: 40,
+    zIndex: 4
   },
-  Input:{
-    width:'100%',
-    height:'80%',
+  Input: {
+    width: "100%",
+    height: "80%"
   },
-  TextInput:{
-    color:'#323232',
-    fontSize:26,
-    fontWeight:'700',
+  TextInput: {
+    color: "#323232",
+    fontSize: 26,
+    fontWeight: "700"
   },
-  ViewFina:{
-    width:'100%',
-    height:'5%',
-    backgroundColor:'#323232'
+  ViewFina: {
+    width: "100%",
+    height: "5%",
+    backgroundColor: "#323232"
   },
-  Botao:{
-    width:'70%',
-    height:'12%',
-    backgroundColor: '#347474',
+  Botao: {
+    width: "70%",
+    height: "12%",
+    backgroundColor: "#347474",
     borderRadius: 10,
-    marginBottom:10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex:4
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 4
   },
-  TextEntrar:{
-    color:'#ffff',
-    fontSize:20,
-    fontWeight:'700',
+  TextEntrar: {
+    color: "#ffff",
+    fontSize: 20,
+    fontWeight: "700"
   },
-  BotaoCadastrar:{
-    width:'70%',
-    height:'12%',
-    backgroundColor: '#ffff',
+  BotaoCadastrar: {
+    width: "70%",
+    height: "12%",
+    backgroundColor: "#ffff",
     borderRadius: 10,
-    marginBottom:10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex:4
+    marginBottom: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    zIndex: 4
   },
-  TextCadastrar:{
-    color:'#323232',
-    fontSize:20,
-    fontWeight:'700',
-  },
-
+  TextCadastrar: {
+    color: "#323232",
+    fontSize: 20,
+    fontWeight: "700"
+  }
 });
